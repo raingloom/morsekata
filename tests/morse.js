b5c2d5c895451e65a71a49b2037ba8fecd1c2e54
Rain Gloom <redkarma@hush.com>
2016-12-16 14:33:11 +0000

    refactored test to methods and renamed morse module to reflect its full scope

---
+++ tests/morse.js
@@ -0,0 +1,24 @@
+'use strict'
+let expect = require("chai").expect;
+
+let Morse = require("../morse");
+
+describe("Morse",function(){
+    describe(".decode",function() {
+        it("should decode the 'a' character",function(){
+            expect(Morse.decode("a")).to.eql(".-");
+        });
+        it("should decode 'lua' as a word",function(){
+            expect(Morse.decode("lua")).to.eql(".-.. ..- .-");
+        });
+        it("should decode 'trailing commas' as two words",function(){
+            expect(Morse.decode("trailing commas")).to.eql("- .-. .- .. .-.. .. -. --.   -.-. --- -- -- .- ...");
+        });
+        it("should treat 'MiXeDcAsE' case insensitively",function(){
+            expect(Morse.decode("MiXeDcAsE")).to.eql("-- .. -..- . -.. -.-. .- ... .");
+        });
+        it("should ignore unknown characters",function(){
+            expect(Morse.decode("!@!$#$%#$%#^")).to.eql("");
+        });
+    })
+});
