da5dd9fc0d612382f34c1badb5ef8b0733393c09
Rain Gloom <redkarma@hush.com>
2016-12-20 14:50:55 +0000

    test for correct whitespace handling

--- tests/morse.js
+++ tests/morse.js
@@ -36,6 +36,13 @@ describe("Morse",function(){
             }
             expect(accum).to.eql([".-..", "..-", ".-"]);
         });
+        it("should handle trailing and leading whitespace correctly",function(){
+            let accum=[];
+            for(let x of Morse.morseStream("  .-.. ..- .-   ")){
+                accum.push(x);
+            }
+            expect(accum).to.eql([".-..", "..-", ".-"]);
+        });
     });
     describe(".decode",function() {
         it("should decode .- as 'A'",function(){
