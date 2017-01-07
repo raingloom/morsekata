dea38a6872610f40207c894796801ac2793f8476
Rain Gloom <redkarma@hush.com>
2017-01-07 20:47:45 +0000

    added test for existing user

--- tests/users.js
+++ tests/users.js
@@ -8,5 +8,15 @@ describe("Users",function(){
         it("should work with username only",function(){
             expect(Users.register("testUser")).to.be.ok;
         });
+        it("should work with username and real name",function(){
+            expect(Users.register("testUser","Mr User of Testonia")).to.be.ok;
+        });
+        it("should not work with real name only",function(){
+            expect(
+                function(){
+                    Users.register("testUser","Mr User of Testonia");
+                }
+            ).to.throw(Users.UserExistsException);
+        });
     });
 });
