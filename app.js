8827520ac49c76ff7ee1b278b609299147c49aa4
Rain Gloom <redkarma@hush.com>
2017-01-15 03:25:50 +0000

    ironed out some bugs in app

--- app.js
+++ app.js
@@ -31,4 +31,20 @@ app.post('/users/:name/:fullname?',function(req, res){
     }
 });
 
+app.post('/users/:username/messages',function(req) {
+    let token = req.header('X-Auth');
+    console.log(token);
+    if(token!==undefined && Session.hasUser(token)) {
+        let user = req.params.username;
+        if(Messages.hasInbox(user)) {
+            //TODO: check message
+            Messages.send(Session.getName(token),user,req.body.message);
+        } else {
+            req.status(404).send();
+        }
+    } else {
+        req.status(401).send();
+    }
+});
+
 module.exports = app;
--- tests/app.js
+++ tests/app.js
@@ -20,44 +20,73 @@ describe("HTTP API",function() {
     before(function() {
         server = app.listen();//let OS decide port and hostname
     });
-    it('should add user by nickname',function(done){
-        chai.request(server)
-            .post("/users/test1")
-            .end((err,res) => {
-                testForOkResponse(res);
-                done();
-            });
+    describe('registration',function() {
+        it('should add user by nickname',function(done){
+            chai.request(server)
+                .post("/users/test1")
+                .end((err,res) => {
+                    testForOkResponse(res);
+                    done();
+                });
+        });
+        it('should add user with nickname and full name',function(done){
+            chai.request(server)
+                .post("/users/test2/Test the Two")
+                .end((err,res) => {
+                    testForOkResponse(res);
+                    done();
+                });
+        });
+        it('should give different tokens for users',function(done){
+            let token1, token2;
+            chai.request(server)
+                .post("/users/testA")
+                .end((err,res) => {
+                    token1=res.body.token;
+                    chai.request(server)
+                        .post("/users/testB")
+                        .end((err,res) => {
+                            token2=res.body.token;
+                            expect(token1).to.not.eql(token2);
+                            //can't really make sure it NEVER happens, but at least we can make sure it doesn't happen in the trivial case
+                            done();
+                        });
+                });
+        });
+        it('should not double add',function(done){
+            chai.request(server)
+                .post("/users/test1")
+                .end((err,res) => {
+                    expect(res).to.have.status(400);
+                    done();
+                });
+        });
     });
-    it('should add user with nickname and full name',function(done){
-        chai.request(server)
-            .post("/users/test2/Test the Two")
-            .end((err,res) => {
-                testForOkResponse(res);
-                done();
-            });
-    });
-    it('should give different tokens for users',function(done){
-        let token1, token2;
-        chai.request(server)
-            .post("/users/testA")
-            .end((err,res) => {
-                token1=res.body.token;
+    
+    let alicetoken, bobtoken;
+    describe('message', function() {
+        before(function(done) {
+            chai.request(server)
+                .post('/users/alice/Al Ice')
+                .end(function(err,res){
+                    alicetoken=res.body.token;
+                    chai.request(server)
+                        .post('/users/bob/Bo Bert')
+                        .end(function(err,res){
+                            bobtoken=res.body.token;
+                            done();
+                        });
+                });
+        })
+        describe('sending', function() {
+            it('should reject without X-Auth', function(done) {
                 chai.request(server)
-                    .post("/users/testB")
+                    .post("/users/alice/messages")
                     .end((err,res) => {
-                        token2=res.body.token;
-                        expect(token1).to.not.eql(token2);
-                        //can't really make sure it NEVER happens, but at least we can make sure it doesn't happen in the trivial case
+                        expect(res).to.have.status(400);
                         done();
                     });
             });
-    });
-    it('should not double add',function(done){
-        chai.request(server)
-            .post("/users/test1")
-            .end((err,res) => {
-                expect(res).to.have.status(400);
-                done();
-            });
+        });
     });
 });
